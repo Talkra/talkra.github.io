0b98441f073a6ca36d206f530f44bf43cf2f4286
Harry Tom <61614900+Harry260@users.noreply.github.com>
2020-10-29 13:03:35 +0530

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -7,7 +7,7 @@ checkUsr();
 
 function validateJoinable(){
     if(codeJoinInput.value !== '' && passJoinInput.value !== '' && nameJoinInput.value !== ''){
-        CheckIfExist(codeJoinInput.value + passJoinInput.value);
+        CheckIfExist(codeJoinInput.value.toLowerCase() + passJoinInput.value.toLowerCase());
     }
     else{
         document.getElementById('room-not-avil-msg').style = 'bottom:25px;left:calc(50% - 142px)';
@@ -37,15 +37,15 @@ function checkUsr() {
     } else {}
 }
 
-function CheckIfExist(ChatRoomId){
+function CheckIfExist(ChatRoomId.toLowerCase()){
     firebase.database().ref('chatroom/' + ChatRoomId ).once('value', function(snapshot){
         try{
-            ShareLink = 'https://talkra.github.io/chat#' + codeJoinInput.value;
+            ShareLink = 'https://talkra.github.io/chat#' + codeJoinInput.value.toLowerCase();
             console.log(snapshot.val().admin + ' Successfully Joined');
-            chatroomId = codeJoinInput.value + passJoinInput.value;
+            chatroomId = codeJoinInput.value.toLowerCase() + passJoinInput.value.toLowerCase();
             u_name = nameJoinInput.value;
             setCookie("username", nameJoinInput.value, 89);
-            document.getElementById('chat-name').textContent = codeJoinInput.value;
+            document.getElementById('chat-name').textContent = '#' + codeJoinInput.value.toLowerCase();
             messageAsync();
             document.getElementById('auth-lay').style = 'visibility:hidden;opacity:0'
             //
