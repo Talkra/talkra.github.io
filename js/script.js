4f8c0af906cb5a37da7a26d753ac4b2a582bab57
Harry Tom <61614900+Harry260@users.noreply.github.com>
2020-10-29 12:57:34 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -101,3 +101,16 @@ function copyUrl(){
     //}
 //});
 
+function Clipboard_CopyTo(value) {
+    var tempInput = document.createElement("input");
+    tempInput.value = value;
+    document.body.appendChild(tempInput);
+    tempInput.select();
+    document.execCommand("copy");
+    document.body.removeChild(tempInput);
+}
+  
+document.querySelector('#ShareBtn').onclick = function() {
+    Clipboard_CopyTo(ShareLink);
+}
+
